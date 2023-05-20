e4126cd185bc2d6195a522bd8d29d4d79d0883bd
Artem Krotov <timmson666@mail.ru>
2023-05-20 10:38:36 +0300

    - Migrate to docs (#7)
    - Add dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,51 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const devMode = process.env.NODE_ENV !== "production";
+const outputDir = path.resolve(__dirname, "./docs");
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        path: path.resolve(__dirname, './'),
-        filename: "index.js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(sa|sc|c)ss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    "sass-loader"
-                ]
-            },
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["@babel/preset-env"]
-                    }
-                }
-            }
-        ]
-    },
-    resolve: {
-        alias: {
-            "vue$": "vue/dist/vue.esm.js"
-        }
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: "index.css"
-        })
+	entry: "./src/index.js",
+	output: {
+		path: outputDir,
+		filename: "index.js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(sa|sc|c)ss$/i,
+				use: [
+					MiniCssExtractPlugin.loader,
+					"css-loader",
+					"sass-loader"
+				]
+			},
+			{
+				test: /\.m?js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env"]
+					}
+				}
+			}
+		]
+	},
+	resolve: {
+		alias: {
+			"vue$": "vue/dist/vue.esm.js"
+		}
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: "index.css"
+		})
 
-    ]
+	],
+	devServer: {
+		static: {
+			directory: outputDir,
+		},
+		compress: true,
+		port: 3000
+	}
 };
